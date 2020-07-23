7aa9312a96886116691640d3b7481f3a175770a4
Matthew Beck <matthew.beck@digital.hmrc.gov.uk>
2020-07-23 14:58:58 +0100

    CTCTRADERS-718: TraderAtDestination XMLReader

--- test/models/TraderAtDestinationSpec.scala
+++ test/models/TraderAtDestinationSpec.scala
@@ -16,21 +16,77 @@
 
 package models
 
+import com.lucidchart.open.xtract.XmlReader
 import generators.ModelGenerators
 import org.scalacheck.Arbitrary.arbitrary
 import org.scalatest.FreeSpec
 import org.scalatest.MustMatchers
+import org.scalatest.OptionValues
 import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
 import play.api.libs.json.JsSuccess
 import play.api.libs.json.Json
 
-class TraderAtDestinationSpec extends FreeSpec with MustMatchers with ScalaCheckPropertyChecks with ModelGenerators {
+import scala.xml.NodeSeq
+
+class TraderAtDestinationSpec extends FreeSpec with MustMatchers with ScalaCheckPropertyChecks with ModelGenerators with OptionValues {
 
   "Trader at Destination" - {
 
-    "must deserialise to a Trader at Destination with an EORI" - {
+    "JSON" - {
 
-      "when all address fields are present" in {
+      "must deserialise to a Trader at Destination with an EORI" - {
+
+        "when all address fields are present" in {
+
+          forAll(arbitrary[String], arbitrary[String], arbitrary[String], arbitrary[String], arbitrary[String], arbitrary[String]) {
+            (eori, name, streetAndNumber, postCode, city, countryCode) =>
+              val json = Json.obj(
+                "eori"            -> eori,
+                "name"            -> name,
+                "streetAndNumber" -> streetAndNumber,
+                "postCode"        -> postCode,
+                "city"            -> city,
+                "countryCode"     -> countryCode
+              )
+
+              val expectedResult = TraderAtDestinationWithEori(eori, Some(name), Some(streetAndNumber), Some(postCode), Some(city), Some(countryCode))
+
+              json.validate[TraderAtDestination] mustEqual JsSuccess(expectedResult)
+          }
+        }
+
+        "when only the EORI is present" in {
+
+          forAll(arbitrary[String]) {
+            eori =>
+              val json = Json.obj("eori" -> eori)
+
+              val expectedResult = TraderAtDestinationWithEori(eori, None, None, None, None, None)
+
+              json.validate[TraderAtDestination] mustEqual JsSuccess(expectedResult)
+          }
+        }
+      }
+
+      "must deserialise to a Trader without EORI" in {
+
+        forAll(arbitrary[String], arbitrary[String], arbitrary[String], arbitrary[String], arbitrary[String]) {
+          (name, streetAndNumber, postCode, city, countryCode) =>
+            val json = Json.obj(
+              "name"            -> name,
+              "streetAndNumber" -> streetAndNumber,
+              "postCode"        -> postCode,
+              "city"            -> city,
+              "countryCode"     -> countryCode
+            )
+
+            val expectedResult = TraderAtDestinationWithoutEori(name, streetAndNumber, postCode, city, countryCode)
+
+            json.validate[TraderAtDestination] mustEqual JsSuccess(expectedResult)
+        }
+      }
+
+      "must serialise from a Trader with EORI" in {
 
         forAll(arbitrary[String], arbitrary[String], arbitrary[String], arbitrary[String], arbitrary[String], arbitrary[String]) {
           (eori, name, streetAndNumber, postCode, city, countryCode) =>
@@ -43,74 +99,81 @@ class TraderAtDestinationSpec extends FreeSpec with MustMatchers with ScalaCheck
               "countryCode"     -> countryCode
             )
 
-            val expectedResult = TraderAtDestinationWithEori(eori, Some(name), Some(streetAndNumber), Some(postCode), Some(city), Some(countryCode))
-
-            json.validate[TraderAtDestination] mustEqual JsSuccess(expectedResult)
+            Json.toJson(
+              TraderAtDestinationWithEori(eori, Some(name), Some(streetAndNumber), Some(postCode), Some(city), Some(countryCode)): TraderAtDestination) mustEqual json
         }
       }
 
-      "when only the EORI is present" in {
+      "must serialise from a Trader without EORI" in {
 
-        forAll(arbitrary[String]) {
-          eori =>
-            val json = Json.obj("eori" -> eori)
+        forAll(arbitrary[String], arbitrary[String], arbitrary[String], arbitrary[String], arbitrary[String]) {
+          (name, streetAndNumber, postCode, city, countryCode) =>
+            val json = Json.obj(
+              "name"            -> name,
+              "streetAndNumber" -> streetAndNumber,
+              "postCode"        -> postCode,
+              "city"            -> city,
+              "countryCode"     -> countryCode
+            )
 
-            val expectedResult = TraderAtDestinationWithEori(eori, None, None, None, None, None)
-
-            json.validate[TraderAtDestination] mustEqual JsSuccess(expectedResult)
+            Json.toJson(TraderAtDestinationWithoutEori(name, streetAndNumber, postCode, city, countryCode): TraderAtDestination) mustEqual json
         }
       }
     }
 
-    "must deserialise to a Trader without EORI" in {
+    "XML" - {
 
-      forAll(arbitrary[String], arbitrary[String], arbitrary[String], arbitrary[String], arbitrary[String]) {
-        (name, streetAndNumber, postCode, city, countryCode) =>
-          val json = Json.obj(
-            "name"            -> name,
-            "streetAndNumber" -> streetAndNumber,
-            "postCode"        -> postCode,
-            "city"            -> city,
-            "countryCode"     -> countryCode
-          )
+      "must deserialise to a Trader at Destination with an EORI" in {
 
-          val expectedResult = TraderAtDestinationWithoutEori(name, streetAndNumber, postCode, city, countryCode)
+        forAll(arbitrary[TraderAtDestinationWithEori]) {
+          trader =>
+            val xml =
+              <TRADESTRD>
+            {
+              trader.name.fold(NodeSeq.Empty) { name =>
+                <NamTRD7>{name}</NamTRD7>
+              } ++
+              trader.streetAndNumber.fold(NodeSeq.Empty) { streetNumber =>
+                <StrAndNumTRD22>{streetNumber}</StrAndNumTRD22>
+              } ++
+              trader.postCode.fold(NodeSeq.Empty) { postcode =>
+                <PosCodTRD23>{postcode}</PosCodTRD23>
+              } ++
+              trader.city.fold(NodeSeq.Empty) { city =>
+                <CitTRD24>{city}</CitTRD24>
+              } ++
+              trader.countryCode.fold(NodeSeq.Empty) { countryCode =>
+                <CouTRD25>{countryCode}</CouTRD25>
+              }
+            }
+            <TINTRD59>{trader.eori}</TINTRD59>
+          </TRADESTRD>
 
-          json.validate[TraderAtDestination] mustEqual JsSuccess(expectedResult)
+            val result = XmlReader.of[TraderAtDestination].read(xml).toOption.value
+
+            result mustBe trader
+        }
+
       }
-    }
 
-    "must serialise from a Trader with EORI" in {
+      "must deserialise to Trader at Destination without an EORI" in {
+        forAll(arbitrary[TraderAtDestinationWithoutEori]) {
+          trader =>
+            val xml =
+              <TRADESTRD>
+                <NamTRD7>{trader.name}</NamTRD7>
+                <StrAndNumTRD22>{trader.streetAndNumber}</StrAndNumTRD22>
+                <PosCodTRD23>{trader.postCode}</PosCodTRD23>
+                <CitTRD24>{trader.city}</CitTRD24>
+                <CouTRD25>{trader.countryCode}</CouTRD25>
+              </TRADESTRD>
 
-      forAll(arbitrary[String], arbitrary[String], arbitrary[String], arbitrary[String], arbitrary[String], arbitrary[String]) {
-        (eori, name, streetAndNumber, postCode, city, countryCode) =>
-          val json = Json.obj(
-            "eori"            -> eori,
-            "name"            -> name,
-            "streetAndNumber" -> streetAndNumber,
-            "postCode"        -> postCode,
-            "city"            -> city,
-            "countryCode"     -> countryCode
-          )
+            val result = XmlReader.of[TraderAtDestination].read(xml).toOption.value
 
-          Json.toJson(TraderAtDestinationWithEori(eori, Some(name), Some(streetAndNumber), Some(postCode), Some(city), Some(countryCode)): TraderAtDestination) mustEqual json
-      }
-    }
-
-    "must serialise from a Trader without EORI" in {
-
-      forAll(arbitrary[String], arbitrary[String], arbitrary[String], arbitrary[String], arbitrary[String]) {
-        (name, streetAndNumber, postCode, city, countryCode) =>
-          val json = Json.obj(
-            "name"            -> name,
-            "streetAndNumber" -> streetAndNumber,
-            "postCode"        -> postCode,
-            "city"            -> city,
-            "countryCode"     -> countryCode
-          )
-
-          Json.toJson(TraderAtDestinationWithoutEori(name, streetAndNumber, postCode, city, countryCode): TraderAtDestination) mustEqual json
+            result mustBe trader
+        }
       }
     }
   }
+
 }
