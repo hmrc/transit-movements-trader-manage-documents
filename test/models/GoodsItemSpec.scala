44b566399e9d82cd4249ffd81354144a74992539
Matthew Beck <matthew.beck@digital.hmrc.gov.uk>
2020-07-28 11:33:46 +0100

    CTCTRADERS-718: Moved repeated XML logic to helper

--- test/models/GoodsItemSpec.scala
+++ test/models/GoodsItemSpec.scala
@@ -18,13 +18,12 @@ package models
 
 import com.lucidchart.open.xtract.XmlReader
 import generators.ModelGenerators
+import org.scalacheck.Arbitrary.arbitrary
 import org.scalatest.FreeSpec
 import org.scalatest.MustMatchers
 import org.scalatest.OptionValues
 import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
-import org.scalacheck.Arbitrary.arbitrary
-
-import scala.xml.NodeSeq
+import utils.XMLBuilderHelper
 
 class GoodsItemSpec extends FreeSpec with MustMatchers with ScalaCheckPropertyChecks with ModelGenerators with OptionValues {
 
@@ -36,57 +35,7 @@ class GoodsItemSpec extends FreeSpec with MustMatchers with ScalaCheckPropertyCh
 
         forAll(arbitrary[GoodsItem]) {
           goodsItem =>
-            val xml = {
-              <GOOITEGDS>
-              <IteNumGDS7>{goodsItem.itemNumber}</IteNumGDS7>
-              {
-                {
-                  goodsItem.commodityCode.fold(NodeSeq.Empty) { commodityCode =>
-                    <ComCodTarCodGDS10>{commodityCode}</ComCodTarCodGDS10>
-                  } ++
-                  goodsItem.declarationType.fold(NodeSeq.Empty) { declarationType =>
-                    <DecTypGDS15>{declarationType.toString}</DecTypGDS15>
-                  }
-                }
-              }
-              <GooDesGDS23>{goodsItem.description}</GooDesGDS23>
-              {
-                goodsItem.grossMass.fold(NodeSeq.Empty) { grossMass =>
-                  <GroMasGDS46>{grossMass}</GroMasGDS46>
-                } ++
-                goodsItem.netMass.fold(NodeSeq.Empty) { netMass =>
-                  <NetMasGDS48>{netMass}</NetMasGDS48>
-                }
-              }
-              <CouOfDisGDS58>{goodsItem.countryOfDispatch}</CouOfDisGDS58>
-              <CouOfDesGDS59>{goodsItem.countryOfDestination}</CouOfDesGDS59>
-              {
-                goodsItem.producedDocuments.map(producedDocumentXML) ++
-                goodsItem.specialMentions.map(specialMentionXML) ++
-                goodsItem.consignor.map(consignorXML) ++
-                goodsItem.consignee.map(consigneeXML) ++
-                goodsItem.containers.map {
-                  containersNumber =>
-                    <CONNR2>
-                      <ConNumNR21>{containersNumber}</ConNumNR21>
-                    </CONNR2>
-                } ++
-                goodsItem.packages.toList.map(packageToXML) ++
-                goodsItem.sensitiveGoodsInformation.map {
-                  sensitiveInformation =>
-                  <SGICODSD2>
-                    {
-                      sensitiveInformation.goodsCode.fold(NodeSeq.Empty) { goodsCode =>
-                        <SenGooCodSD22>{goodsCode}</SenGooCodSD22>
-                      }
-                    }
-                    <SenQuaSD23>{sensitiveInformation.quantity}</SenQuaSD23>
-                  </SGICODSD2>
-                }
-              }
-            </GOOITEGDS>
-            }
-
+            val xml    = XMLBuilderHelper.goodsItemToXml(goodsItem)
             val result = XmlReader.of[GoodsItem].read(xml).toOption.value
 
             result mustBe goodsItem
@@ -103,98 +52,4 @@ class GoodsItemSpec extends FreeSpec with MustMatchers with ScalaCheckPropertyCh
       }
     }
   }
-
-  private def producedDocumentXML(producedDocument: ProducedDocument): NodeSeq =
-    <PRODOCDC2>
-      <DocTypDC21>{producedDocument.documentType}</DocTypDC21>
-      {
-      producedDocument.reference.fold(NodeSeq.Empty) { reference =>
-        <DocRefDC23>{reference}</DocRefDC23>
-      } ++
-        producedDocument.complementOfInformation.fold(NodeSeq.Empty) { information =>
-          <ComOfInfDC25>{information}</ComOfInfDC25>
-        }
-      }
-    </PRODOCDC2>
-
-  private def specialMentionXML(specialMention: SpecialMention): NodeSeq =
-    <SPEMENMT2>
-    {
-      specialMention match {
-        case value: SpecialMentionEc =>
-          <ExpFroECMT24>1</ExpFroECMT24>
-          <AddInfCodMT23>{value.additionalInformationCoded}</AddInfCodMT23>
-        case value: SpecialMentionNonEc =>
-          <ExpFroECMT24>0</ExpFroECMT24>
-          <AddInfCodMT23>{value.additionalInformationCoded}</AddInfCodMT23>
-          <ExpFroCouMT25>{value.exportFromCountry}</ExpFroCouMT25>
-        case value: SpecialMentionNoCountry =>
-          <AddInfCodMT23>{value.additionalInformationCoded}</AddInfCodMT23>
-      }
-    }
-    </SPEMENMT2>
-
-  private def consignorXML(consignor: Consignor): NodeSeq =
-    <TRACONCO2>
-      <NamCO17>{consignor.name}</NamCO17>
-      <StrAndNumCO122>{consignor.streetAndNumber}</StrAndNumCO122>
-      <PosCodCO123>{consignor.postCode}</PosCodCO123>
-      <CitCO124>{consignor.city}</CitCO124>
-      <CouCO125>{consignor.countryCode}</CouCO125>
-      {
-      consignor.nadLanguageCode.fold(NodeSeq.Empty) { nadLangCode =>
-        <NADLNGCO>{nadLangCode}</NADLNGCO>
-      } ++
-        consignor.eori.fold(NodeSeq.Empty) { eori =>
-          <TINCO159>{eori}</TINCO159>
-        }
-      }
-    </TRACONCO2>
-
-  private def consigneeXML(consignee: Consignee): NodeSeq =
-    <TRACONCE2>
-      <NamCE17>{consignee.name}</NamCE17>
-      <StrAndNumCE122>{consignee.streetAndNumber}</StrAndNumCE122>
-      <PosCodCE123>{consignee.postCode}</PosCodCE123>
-      <CitCE124>{consignee.city}</CitCE124>
-      <CouCE125>{consignee.countryCode}</CouCE125>
-      {
-      consignee.nadLanguageCode.fold(NodeSeq.Empty) { nadLangCode =>
-        <NADLNGCE>{nadLangCode}</NADLNGCE>
-      } ++
-        consignee.eori.fold(NodeSeq.Empty) { eori =>
-          <TINCE159>{eori}</TINCE159>
-        }
-      }
-    </TRACONCE2>
-
-  private def packageToXML(packageModel: Package): NodeSeq =
-    packageModel match {
-      case value: UnpackedPackage =>
-        <PACGS2>
-          <KinOfPacGS23>{value.kindOfPackage}</KinOfPacGS23>
-          <NumOfPieGS25>{value.numberOfPieces}</NumOfPieGS25>
-          {
-            value.marksAndNumbers.fold(NodeSeq.Empty) { marksAndNumbers =>
-              <MarNumOfPacGS21>{marksAndNumbers}</MarNumOfPacGS21>
-            }
-          }
-        </PACGS2>
-      case value: RegularPackage =>
-        <PACGS2>
-          <KinOfPacGS23>{value.kindOfPackage}</KinOfPacGS23>
-          <NumOfPacGS24>{value.numberOfPackages}</NumOfPacGS24>
-          <MarNumOfPacGS21>{value.marksAndNumbers}</MarNumOfPacGS21>
-        </PACGS2>
-      case value: BulkPackage =>
-        <PACGS2>
-          <KinOfPacGS23>{value.kindOfPackage}</KinOfPacGS23>
-          {
-            value.marksAndNumbers.fold(NodeSeq.Empty) { marksAndNumbers =>
-              <MarNumOfPacGS21>{marksAndNumbers}</MarNumOfPacGS21>
-            }
-          }
-        </PACGS2>
-    }
-
 }
---
+++ test/utils/XMLBuilderHelper.scala
@@ -0,0 +1,231 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package utils
+
+import models.BulkPackage
+import models.Consignee
+import models.Consignor
+import models.GoodsItem
+import models.Package
+import models.ProducedDocument
+import models.RegularPackage
+import models.SpecialMention
+import models.SpecialMentionEc
+import models.SpecialMentionNoCountry
+import models.SpecialMentionNonEc
+import models.TraderAtDestination
+import models.TraderAtDestinationWithEori
+import models.TraderAtDestinationWithoutEori
+import models.UnpackedPackage
+
+import scala.xml.NodeSeq
+
+object XMLBuilderHelper {
+
+  def goodsItemToXml(goodsItem: GoodsItem) =
+    <GOOITEGDS>
+      <IteNumGDS7>{goodsItem.itemNumber}</IteNumGDS7>
+      {
+      {
+        goodsItem.commodityCode.fold(NodeSeq.Empty) { commodityCode =>
+          <ComCodTarCodGDS10>{commodityCode}</ComCodTarCodGDS10>
+        } ++
+          goodsItem.declarationType.fold(NodeSeq.Empty) { declarationType =>
+            <DecTypGDS15>{declarationType.toString}</DecTypGDS15>
+          }
+      }
+      }
+      <GooDesGDS23>{goodsItem.description}</GooDesGDS23>
+      {
+      goodsItem.grossMass.fold(NodeSeq.Empty) { grossMass =>
+        <GroMasGDS46>{grossMass}</GroMasGDS46>
+      } ++
+        goodsItem.netMass.fold(NodeSeq.Empty) { netMass =>
+          <NetMasGDS48>{netMass}</NetMasGDS48>
+        }
+      }
+      <CouOfDisGDS58>{goodsItem.countryOfDispatch}</CouOfDisGDS58>
+      <CouOfDesGDS59>{goodsItem.countryOfDestination}</CouOfDesGDS59>
+      {
+      goodsItem.producedDocuments.map(producedDocumentXML) ++
+        goodsItem.specialMentions.map(specialMentionXML) ++
+        goodsItem.consignor.map(consignorXML) ++
+        goodsItem.consignee.map(consigneeXML) ++
+        goodsItem.containers.map {
+          containersNumber =>
+            <CONNR2>
+              <ConNumNR21>{containersNumber}</ConNumNR21>
+            </CONNR2>
+        } ++
+        goodsItem.packages.toList.map(packageToXML) ++
+        goodsItem.sensitiveGoodsInformation.map {
+          sensitiveInformation =>
+            <SGICODSD2>
+              {
+              sensitiveInformation.goodsCode.fold(NodeSeq.Empty) { goodsCode =>
+                <SenGooCodSD22>{goodsCode}</SenGooCodSD22>
+              }
+              }
+              <SenQuaSD23>{sensitiveInformation.quantity}</SenQuaSD23>
+            </SGICODSD2>
+        }
+      }
+    </GOOITEGDS>
+
+  def traderAtDestinationToXml(traderAtDestination: TraderAtDestination): NodeSeq =
+    traderAtDestination match {
+      case value: TraderAtDestinationWithEori =>
+        <TINTRD59>{value.eori}</TINTRD59> ++ {
+          value.name.fold(NodeSeq.Empty) {
+            name =>
+              <NamTRD7>{name}</NamTRD7>
+          } ++
+            value.streetAndNumber.fold(NodeSeq.Empty) {
+              streetAndNumber =>
+                <StrAndNumTRD22>{streetAndNumber}</StrAndNumTRD22>
+            } ++
+            value.postCode.fold(NodeSeq.Empty) {
+              postcode =>
+                <PosCodTRD23>{postcode}</PosCodTRD23>
+            } ++
+            value.city.fold(NodeSeq.Empty) {
+              city =>
+                <CitTRD24>{city}</CitTRD24>
+            } ++
+            value.countryCode.fold(NodeSeq.Empty) {
+              countryCode =>
+                <CouTRD25>{countryCode}</CouTRD25>
+            }
+        }
+      case value: TraderAtDestinationWithoutEori =>
+        <NamTRD7>{value.name}</NamTRD7>
+          <StrAndNumTRD22>{value.streetAndNumber}</StrAndNumTRD22>
+          <PosCodTRD23>{value.postCode}</PosCodTRD23>
+          <CitTRD24>{value.city}</CitTRD24>
+          <CouTRD25>{value.countryCode}</CouTRD25>
+    }
+
+  def sealsToXml(seals: Seq[String]): NodeSeq =
+    seals match {
+      case seals: Seq[String] if seals.nonEmpty =>
+        <SEAINFSLI>
+          {
+          seals.map {
+            sealId =>
+              <SEAIDSID>
+                <SeaIdeSID1>{sealId}</SeaIdeSID1>
+              </SEAIDSID>
+          }
+          }
+        </SEAINFSLI>
+      case _ => NodeSeq.Empty
+    }
+
+  def producedDocumentXML(producedDocument: ProducedDocument): NodeSeq =
+    <PRODOCDC2>
+      <DocTypDC21>{producedDocument.documentType}</DocTypDC21>
+      {
+      producedDocument.reference.fold(NodeSeq.Empty) { reference =>
+        <DocRefDC23>{reference}</DocRefDC23>
+      } ++
+        producedDocument.complementOfInformation.fold(NodeSeq.Empty) { information =>
+          <ComOfInfDC25>{information}</ComOfInfDC25>
+        }
+      }
+    </PRODOCDC2>
+
+  def specialMentionXML(specialMention: SpecialMention): NodeSeq =
+    <SPEMENMT2>
+      {
+      specialMention match {
+        case value: SpecialMentionEc =>
+          <ExpFroECMT24>1</ExpFroECMT24>
+            <AddInfCodMT23>{value.additionalInformationCoded}</AddInfCodMT23>
+        case value: SpecialMentionNonEc =>
+          <ExpFroECMT24>0</ExpFroECMT24>
+            <AddInfCodMT23>{value.additionalInformationCoded}</AddInfCodMT23>
+            <ExpFroCouMT25>{value.exportFromCountry}</ExpFroCouMT25>
+        case value: SpecialMentionNoCountry =>
+          <AddInfCodMT23>{value.additionalInformationCoded}</AddInfCodMT23>
+      }
+      }
+    </SPEMENMT2>
+
+  def consignorXML(consignor: Consignor): NodeSeq =
+    <TRACONCO2>
+      <NamCO17>{consignor.name}</NamCO17>
+      <StrAndNumCO122>{consignor.streetAndNumber}</StrAndNumCO122>
+      <PosCodCO123>{consignor.postCode}</PosCodCO123>
+      <CitCO124>{consignor.city}</CitCO124>
+      <CouCO125>{consignor.countryCode}</CouCO125>
+      {
+      consignor.nadLanguageCode.fold(NodeSeq.Empty) { nadLangCode =>
+        <NADLNGCO>{nadLangCode}</NADLNGCO>
+      } ++
+        consignor.eori.fold(NodeSeq.Empty) { eori =>
+          <TINCO159>{eori}</TINCO159>
+        }
+      }
+    </TRACONCO2>
+
+  def consigneeXML(consignee: Consignee): NodeSeq =
+    <TRACONCE2>
+      <NamCE17>{consignee.name}</NamCE17>
+      <StrAndNumCE122>{consignee.streetAndNumber}</StrAndNumCE122>
+      <PosCodCE123>{consignee.postCode}</PosCodCE123>
+      <CitCE124>{consignee.city}</CitCE124>
+      <CouCE125>{consignee.countryCode}</CouCE125>
+      {
+      consignee.nadLanguageCode.fold(NodeSeq.Empty) { nadLangCode =>
+        <NADLNGCE>{nadLangCode}</NADLNGCE>
+      } ++
+        consignee.eori.fold(NodeSeq.Empty) { eori =>
+          <TINCE159>{eori}</TINCE159>
+        }
+      }
+    </TRACONCE2>
+
+  def packageToXML(packageModel: Package): NodeSeq =
+    packageModel match {
+      case value: UnpackedPackage =>
+        <PACGS2>
+          <KinOfPacGS23>{value.kindOfPackage}</KinOfPacGS23>
+          <NumOfPieGS25>{value.numberOfPieces}</NumOfPieGS25>
+            {
+            value.marksAndNumbers.fold(NodeSeq.Empty) { marksAndNumbers =>
+              <MarNumOfPacGS21>{marksAndNumbers}</MarNumOfPacGS21>
+            }
+          }
+        </PACGS2>
+      case value: RegularPackage =>
+        <PACGS2>
+          <KinOfPacGS23>{value.kindOfPackage}</KinOfPacGS23>
+          <NumOfPacGS24>{value.numberOfPackages}</NumOfPacGS24>
+          <MarNumOfPacGS21>{value.marksAndNumbers}</MarNumOfPacGS21>
+        </PACGS2>
+      case value: BulkPackage =>
+        <PACGS2>
+          <KinOfPacGS23>{value.kindOfPackage}</KinOfPacGS23>
+            {
+            value.marksAndNumbers.fold(NodeSeq.Empty) { marksAndNumbers =>
+              <MarNumOfPacGS21>{marksAndNumbers}</MarNumOfPacGS21>
+            }
+          }
+        </PACGS2>
+    }
+
+}
