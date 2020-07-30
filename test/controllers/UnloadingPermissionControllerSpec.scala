c802e1b141194090a5ed9b616277f4eda119db96
Matthew Beck <matthew.beck@digital.hmrc.gov.uk>
2020-07-30 11:33:39 +0100

    CTCTRADERS-718: UnloadingPermissionControllers tests

--- test/controllers/UnloadingPermissionControllerSpec.scala
+++ test/controllers/UnloadingPermissionControllerSpec.scala
@@ -16,21 +16,253 @@
 
 package controllers
 
+import cats.data._
+import cats.data.Validated.Invalid
+import cats.data.Validated.Valid
+import generators.ModelGenerators
+import org.mockito.Matchers.any
+import org.mockito.Mockito.when
+import org.scalacheck.Arbitrary.arbitrary
 import org.scalatest.FreeSpec
 import org.scalatest.MustMatchers
 import org.scalatest.OptionValues
 import org.scalatestplus.mockito.MockitoSugar
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+import play.api.Configuration
+import play.api.inject.bind
+import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.mvc.Call
+import play.api.test.FakeRequest
+import play.api.test.Helpers.route
+import play.api.test.Helpers.status
+import play.api.test.Helpers._
+import services.ConversionService
+import services.PdfGenerator
+import services.ReferenceDataRetrievalError
 
-class UnloadingPermissionControllerSpec extends FreeSpec with MustMatchers with GuiceOneAppPerSuite with OptionValues with MockitoSugar {
+import scala.concurrent.Future
+
+class UnloadingPermissionControllerSpec
+    extends FreeSpec
+    with MustMatchers
+    with GuiceOneAppPerSuite
+    with OptionValues
+    with MockitoSugar
+    with ScalaCheckPropertyChecks
+    with ModelGenerators {
+
+  def onwardRoute: Call = Call("GET", "/foo")
+
+  lazy val unloadingPermissionControllerRoute: String = routes.UnloadingPermissionController.get().url
+
+  val mockCoversionService: ConversionService = mock[ConversionService]
+  val mockPDFGenerator: PdfGenerator          = mock[PdfGenerator]
 
   "get" - {
 
-    "must return OK and PDF" in {}
+    def applicationBuilder: GuiceApplicationBuilder =
+      new GuiceApplicationBuilder()
+        .configure(Configuration("metrics.enabled" -> "false"))
 
-    "must return a BadRequest when given an invalid XMl" in {}
+    "must return OK and PDF" in {
 
-    "must return and InternalServerError if the conversion fails" in {}
+      forAll(arbitrary[viewmodels.PermissionToStartUnloading], arbitrary[Array[Byte]]) {
+        (permissionToStartUnloadingViewModel, pdf) =>
+          val application = applicationBuilder
+            .overrides {
+              bind[ConversionService].toInstance(mockCoversionService)
+              bind[PdfGenerator].toInstance(mockPDFGenerator)
+            }
+            .build()
 
+          when(mockCoversionService.convertUnloadingPermission(any())(any(), any()))
+            .thenReturn(Future.successful(Valid(permissionToStartUnloadingViewModel)))
+
+          when(mockPDFGenerator.generateUnloadingPermission(any()))
+            .thenReturn(pdf)
+
+          running(application) {
+            val request = FakeRequest(POST, unloadingPermissionControllerRoute).withXmlBody(validUnloadingPermissionXml)
+
+            val result = route(application, request).value
+
+            status(result) mustEqual OK
+          }
+      }
+    }
+
+    "must return a BadRequest when given an invalid XML" in {
+
+      val application = applicationBuilder
+        .overrides {
+          bind[ConversionService].toInstance(mockCoversionService)
+          bind[PdfGenerator].toInstance(mockPDFGenerator)
+        }
+        .build()
+
+      running(application) {
+        val request = FakeRequest(POST, unloadingPermissionControllerRoute).withXmlBody(<invalid></invalid>)
+
+        val result = route(application, request).value
+
+        status(result) mustEqual BAD_REQUEST
+      }
+
+    }
+
+    "must return and InternalServerError if the conversion fails" in {
+
+      val application = applicationBuilder
+        .overrides {
+          bind[ConversionService].toInstance(mockCoversionService)
+          bind[PdfGenerator].toInstance(mockPDFGenerator)
+        }
+        .build()
+
+      when(mockCoversionService.convertUnloadingPermission(any())(any(), any()))
+        .thenReturn(Future.successful(Invalid(NonEmptyChain(ReferenceDataRetrievalError("", 500, "")))))
+
+      running(application) {
+        val request = FakeRequest(POST, unloadingPermissionControllerRoute).withXmlBody(validUnloadingPermissionXml)
+
+        val result = route(application, request).value
+
+        status(result) mustEqual OK
+      }
+    }
   }
+
+  private def validUnloadingPermissionXml =
+    <CC043A>
+      <SynIdeMES1>UNOC</SynIdeMES1>
+      <SynVerNumMES2>3</SynVerNumMES2>
+      <MesSenMES3>NTA.GB</MesSenMES3>
+      <MesRecMES6>SYST17B-NCTS_EU_EXIT</MesRecMES6>
+      <DatOfPreMES9>20200729</DatOfPreMES9>
+      <TimOfPreMES10>1058</TimOfPreMES10>
+      <IntConRefMES11>66390912144854</IntConRefMES11>
+      <AppRefMES14>NCTS</AppRefMES14>
+      <TesIndMES18>0</TesIndMES18>
+      <MesIdeMES19>66390912144854</MesIdeMES19>
+      <MesTypMES20>GB043A</MesTypMES20>
+      <HEAHEA>
+        <DocNumHEA5>99IT9876AB88901209</DocNumHEA5>
+        <TypOfDecHEA24>T1</TypOfDecHEA24>
+        <CouOfDesCodHEA30>GB</CouOfDesCodHEA30>
+        <CouOfDisCodHEA55>IT</CouOfDisCodHEA55>
+        <IdeOfMeaOfTraAtDHEA78>abcd</IdeOfMeaOfTraAtDHEA78>
+        <NatOfMeaOfTraAtDHEA80>IT</NatOfMeaOfTraAtDHEA80>
+        <ConIndHEA96>0</ConIndHEA96>
+        <AccDatHEA158>20190912</AccDatHEA158>
+        <TotNumOfIteHEA305>1</TotNumOfIteHEA305>
+        <TotNumOfPacHEA306>1</TotNumOfPacHEA306>
+        <TotGroMasHEA307>1000</TotGroMasHEA307>
+      </HEAHEA>
+      <TRAPRIPC1>
+        <NamPC17>Mancini Carriers</NamPC17>
+        <StrAndNumPC122>90 Desio Way</StrAndNumPC122>
+        <PosCodPC123>MOD 5JJ</PosCodPC123>
+        <CitPC124>Modena</CitPC124>
+        <CouPC125>IT</CouPC125>
+        <TINPC159>IT444100201000</TINPC159>
+      </TRAPRIPC1>
+      <TRACONCO1>
+        <NamCO17>Mancini Carriers</NamCO17>
+        <StrAndNumCO122>90 Desio Way</StrAndNumCO122>
+        <PosCodCO123>MOD 5JJ</PosCodCO123>
+        <CitCO124>Modena</CitCO124>
+        <CouCO125>IT</CouCO125>
+        <TINCO159>IT444100201000</TINCO159>
+      </TRACONCO1>
+      <TRACONCE1>
+        <NamCE17>Mancini Carriers</NamCE17>
+        <StrAndNumCE122>90 Desio Way</StrAndNumCE122>
+        <PosCodCE123>MOD 5JJ</PosCodCE123>
+        <CitCE124>Modena</CitCE124>
+        <CouCE125>IT</CouCE125>
+        <TINCE159>IT444100201000</TINCE159>
+      </TRACONCE1>
+      <TRADESTRD>
+        <NamTRD7>The Luggage Carriers</NamTRD7>
+        <StrAndNumTRD22>225 Suedopolish Yard,</StrAndNumTRD22>
+        <PosCodTRD23>SS8 2BB</PosCodTRD23>
+        <CitTRD24>,</CitTRD24>
+        <CouTRD25>GB</CouTRD25>
+        <TINTRD59>GB163910077000</TINTRD59>
+      </TRADESTRD>
+      <CUSOFFDEPEPT>
+        <RefNumEPT1>IT021100</RefNumEPT1>
+      </CUSOFFDEPEPT>
+      <CUSOFFPREOFFRES>
+        <RefNumRES1>GB000060</RefNumRES1>
+      </CUSOFFPREOFFRES>
+      <SEAINFSLI>
+        <SeaNumSLI2>3</SeaNumSLI2>
+        <SEAIDSID>
+          <SeaIdeSID1>Seals01</SeaIdeSID1>
+        </SEAIDSID>
+        <SEAIDSID>
+          <SeaIdeSID1>Seals02</SeaIdeSID1>
+        </SEAIDSID>
+        <SEAIDSID>
+          <SeaIdeSID1>Seals03</SeaIdeSID1>
+        </SEAIDSID>
+      </SEAINFSLI>
+      <GOOITEGDS>
+        <IteNumGDS7>1</IteNumGDS7>
+        <GooDesGDS23>Flowers</GooDesGDS23>
+        <GroMasGDS46>1000</GroMasGDS46>
+        <NetMasGDS48>999</NetMasGDS48>
+        <CouOfDisGDS58>GB</CouOfDisGDS58>
+        <CouOfDesGDS59>GB</CouOfDesGDS59>
+        <PRODOCDC2>
+          <DocTypDC21>235</DocTypDC21>
+          <DocRefDC23>Ref.</DocRefDC23>
+        </PRODOCDC2>
+        <CONNR2>
+          <ConNumNR21>container 1</ConNumNR21>
+        </CONNR2>
+        <CONNR2>
+          <ConNumNR21>container 2</ConNumNR21>
+        </CONNR2>
+        <PACGS2>
+          <MarNumOfPacGS21>Ref.</MarNumOfPacGS21>
+          <KinOfPacGS23>BX</KinOfPacGS23>
+          <NumOfPacGS24>1</NumOfPacGS24>
+        </PACGS2>
+        <SGICODSD2>
+          <SenGooCodSD22>1</SenGooCodSD22>
+          <SenQuaSD23>1</SenQuaSD23>
+        </SGICODSD2>
+      </GOOITEGDS>
+      <GOOITEGDS>
+        <IteNumGDS7>1</IteNumGDS7>
+        <GooDesGDS23>Flowers</GooDesGDS23>
+        <GroMasGDS46>1000</GroMasGDS46>
+        <NetMasGDS48>999</NetMasGDS48>
+        <CouOfDisGDS58>GB</CouOfDisGDS58>
+        <CouOfDesGDS59>GB</CouOfDesGDS59>
+        <PRODOCDC2>
+          <DocTypDC21>235</DocTypDC21>
+          <DocRefDC23>Ref.</DocRefDC23>
+        </PRODOCDC2>
+        <CONNR2>
+          <ConNumNR21>container 1</ConNumNR21>
+        </CONNR2>
+        <CONNR2>
+          <ConNumNR21>container 2</ConNumNR21>
+        </CONNR2>
+        <PACGS2>
+          <MarNumOfPacGS21>Ref.</MarNumOfPacGS21>
+          <KinOfPacGS23>BX</KinOfPacGS23>
+          <NumOfPacGS24>1</NumOfPacGS24>
+        </PACGS2>
+        <SGICODSD2>
+          <SenGooCodSD22>1</SenGooCodSD22>
+          <SenQuaSD23>1</SenQuaSD23>
+        </SGICODSD2>
+      </GOOITEGDS>
+    </CC043A>
+
 }
