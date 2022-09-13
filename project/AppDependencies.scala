5c7c28b08b01977a397cafd93a0a7a14a57f1a9b
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2022-09-13 22:59:35 +0100

    CTCP-1200: Removed dependencies.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,8 +6,6 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % "7.3.0",
     "com.dmanchester"         %% "playfop"                    % "1.0",
-    "net.sf.barcode4j"        %  "barcode4j"                  % "2.1",
-    "net.sf.barcode4j"        %  "barcode4j-fop-ext"          % "2.1",
     "com.lucidchart"          %% "xtract"                     % "2.2.1"
   )
 
