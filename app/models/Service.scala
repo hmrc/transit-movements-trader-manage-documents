2f4fd4e9f1d3c76392cd280200aee9c8a743b286
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2023-01-05 10:32:58 +0000

    2023 license updates.

--- app/models/Service.scala
+++ app/models/Service.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 HM Revenue & Customs
+ * Copyright 2023 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,8 +18,6 @@ package models
 
 import play.api.ConfigLoader
 import play.api.Configuration
-
-import scala.language.implicitConversions
 
 final case class Service(host: String, port: String, protocol: String, startUrl: String) {
 
