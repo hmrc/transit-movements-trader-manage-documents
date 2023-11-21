f65b2a06dcb1b13c95758ee5b66be47a3dc96807
jackheslop96 <43777106+jackheslop96@users.noreply.github.com>
2023-11-21 17:05:09 +0000

    CTCP-4468: Added xalan to compile dependencies.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -12,7 +12,9 @@ object AppDependencies {
     "com.dmanchester"         %% "playfop"                    % "1.0",
     "net.sf.barcode4j"        %  "barcode4j"                  % "2.1",
     "net.sf.barcode4j"        %  "barcode4j-fop-ext"          % "2.1",
-    "com.lucidchart"          %% "xtract"                     % "2.2.1"
+    "com.lucidchart"          %% "xtract"                     % "2.2.1",
+    "xalan"                   %  "xalan"                      % xalanVersion,
+    "xalan"                   %  "serializer"                 % xalanVersion
   )
 
   val test: Seq[ModuleID] = Seq(
@@ -33,6 +35,7 @@ object AppDependencies {
 
   val overrides: Seq[ModuleID] = Seq(
     "org.typelevel"          %% "cats-core"           % catsVersion,
-    "org.typelevel"          %% "cats-kernel"         % catsVersion
+    "org.typelevel"          %% "cats-kernel"         % catsVersion,
+    "org.apache.xmlgraphics" %  "xmlgraphics-commons" % "2.7"
   )
 }
