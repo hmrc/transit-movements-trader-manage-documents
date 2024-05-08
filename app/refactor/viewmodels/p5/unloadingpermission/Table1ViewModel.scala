0f7d3d7b983f05036eb02e24d3e9e90bfe9f7aa7
joecefnhedog <joecefnhedog@gmail.com>
2024-05-08 11:50:03 +0100

    CTCP-5172: zipWith update

--- app/refactor/viewmodels/p5/unloadingpermission/Table1ViewModel.scala
+++ app/refactor/viewmodels/p5/unloadingpermission/Table1ViewModel.scala
@@ -35,7 +35,7 @@ case class Table1ViewModel(
   security: String,
   inlandModeOfTransport: String,
   departureTransportMeans: Seq[String],
-  houseConsignmentDepartureTransportMeans: Map[Int, Seq[String]],
+  houseConsignmentDepartureTransportMeans: Seq[generated.p5.HouseConsignmentType04],
   container: String,
   transportEquipment: String,
   seals: String,
@@ -67,9 +67,7 @@ object Table1ViewModel {
       inlandModeOfTransport = ie043.Consignment.flatMap(_.inlandModeOfTransport).orElseBlank,
       departureTransportMeans = ie043.Consignment.fold[Seq[String]](Nil)(_.DepartureTransportMeans.map(_.asString)),
       houseConsignmentDepartureTransportMeans = ie043.Consignment
-        .fold[Map[Int, Seq[String]]](Map.empty)(_.HouseConsignment.zipWithIndex.map {
-          case (house, houseIndex) => (houseIndex, house.DepartureTransportMeans.map(_.asString))
-        }.toMap),
+        .fold[Seq[generated.p5.HouseConsignmentType04]](Seq.empty)(_.HouseConsignment),
       container = ie043.Consignment.map(_.containerIndicator.asString).orElseBlank,
       transportEquipment = ie043.Consignment.fold[Seq[String]](Nil)(_.TransportEquipment.map(_.asString)).semiColonSeparate,
       seals = ie043.Consignment.fold[Seq[String]](Nil)(_.TransportEquipment.flatMap(_.Seal).map(_.asString)).semiColonSeparate,
