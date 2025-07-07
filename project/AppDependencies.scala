33a18128779a4be86ea88cb76d48d4fb8ead3193
jackheslop96 <43777106+jackheslop96@users.noreply.github.com>
2025-07-07 15:11:35 +0100

    CTCP-6571: Dependency updates.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,32 +3,32 @@ import sbt.librarymanagement.InclExclRule
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.12.0"
+  private val bootstrapVersion = "9.14.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-30"  % bootstrapVersion,
-    "org.apache.xmlgraphics"  %  "fop"                        % "2.10",
+    "org.apache.xmlgraphics"  %  "fop"                        % "2.11",
     "net.sf.barcode4j"        %  "barcode4j"                  % "2.1",
     "net.sf.barcode4j"        %  "barcode4j-fop-ext"          % "2.1",
     "xerces"                  %  "xercesImpl"                 % "2.12.2",
     "javax.xml.bind"          %  "jaxb-api"                   % "2.3.1",
-    "org.typelevel"           %% "cats-core"                  % "2.12.0"
+    "org.typelevel"           %% "cats-core"                  % "2.13.0"
   )
 
   val test: Seq[ModuleID] = Seq(
     "org.scalatest"           %% "scalatest"                % "3.2.19",
-    "org.scalatestplus.play"  %% "scalatestplus-play"       % "7.0.1",
-    "org.mockito"             %  "mockito-core"             % "5.11.0",
+    "org.scalatestplus.play"  %% "scalatestplus-play"       % "7.0.2",
+    "org.mockito"             %  "mockito-core"             % "5.18.0",
     "org.scalatestplus"       %% "mockito-4-11"             % "3.2.18.0",
     "org.scalatestplus"       %% "scalacheck-1-17"          % "3.2.18.0",
-    "org.apache.pdfbox"       %  "pdfbox"                   % "2.0.30",
-    "org.jsoup"               %  "jsoup"                    % "1.18.1",
+    "org.apache.pdfbox"       %  "pdfbox"                   % "3.0.5",
+    "org.jsoup"               %  "jsoup"                    % "1.21.1",
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"   % bootstrapVersion
   ).map(_ % "test")
 
   val overrides: Seq[ModuleID] = Seq(
     "avalon-framework" % "avalon-framework-impl" % "4.3",
-    "commons-io"       % "commons-io"            % "2.18.0"
+    "commons-io"       % "commons-io"            % "2.19.0"
   )
 
   val exclusions: Seq[InclExclRule] = Seq(
